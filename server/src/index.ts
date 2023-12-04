288834411c2130ab14e3a1fd13bccc12792cccf7
Arthur <arthur.alves.lima@live.com.com>
2023-12-03 23:28:26 -0300

    build: change lib versions and tsconfig file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,37 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { schema } from './Schema';
 import cors from 'cors';
+import { DataSource } from 'typeorm';
+import  dotEnv from 'dotenv';
 
+dotEnv.config();
+  
 const main = async () => {
+    const AppDataSource =  new DataSource({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE,
+        synchronize: false,
+        logging: true,
+        entities: []
+    });
+
+    //    const result =  await AppDataSource.initialize()
+//    console.log(result)
 
     const app = express();
     app.use(cors());
     app.use(express.json());
+    
+    app.use("/graphql", graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    }));
+    
 
     app.listen(3001, () => {
         console.log("Server started on port 3001");
