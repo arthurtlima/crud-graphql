b9590f8590b916e912083ebb73e3e366a53715c4
Arthur <arthur.alves.lima@live.com.com>
2023-12-05 22:38:19 -0300

    feat: create UPDATEING_PASSWORD  mutation

--- server/src/Schema/index.ts
+++ server/src/Schema/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql'
 import { GET_ALL_USERS } from './Queries/User'
-import { CREATE_USER, DELETE_USER } from './Mutations/User'
+import { CREATE_USER, DELETE_USER, UPDATE_PASSWORD } from './Mutations/User'
 
 
 const RootQuery = new GraphQLObjectType({
@@ -15,6 +15,7 @@ const Mutation = new GraphQLObjectType({
     fields: {
         createUser: CREATE_USER,
         deleteUser: DELETE_USER,
+        updatePassword: UPDATE_PASSWORD,
     }
 });
 
