db89934b9e3d3714ec961ffb3c89da9edf32d21b
Arthur <arthur.alves.lima@live.com.com>
2023-12-08 00:41:48 -0300

    feat: create client project, docker configuration

---
+++ mysql/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS notesdb;
+CREATE TABLE IF NOT EXISTS `notesdb`.`users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
---
+++ server/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Instale as dependências usando npm install
+npm install
+
+# Fazendo o build
+npm run build
+
+# Inicie seu aplicativo Node.js
+npm run start
